be26346d12f1381c0510b63da7c714d7ca5e1734
jmkenny94 <jmkenny94@gmail.com>
2023-06-21 16:34:19 -0400

    Started Footer

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -3,7 +3,16 @@ import './footer.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer__section section__padding'>
+      
+      <div className='footer__section-heading section__padding'>
+        <h1 className='gradient__text'>Do you want to step into the <br /> future before others? </h1>
+      </div>
+
+      <div className='footer__btn'>
+          <button type='button'>Request Early Access</button>
+      </div>
+    </div>
   )
 }
 
