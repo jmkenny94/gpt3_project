2d7720f70a0afe25893e2b775a572f730fb87e54
jmkenny94 <jmkenny94@gmail.com>
2023-06-22 14:27:38 -0400

    Finished Footer - finished project

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './footer.css'
+import gpt3Logo from '../../assets/logo.svg';
 
 const Footer = () => {
   return (
@@ -11,6 +12,42 @@ const Footer = () => {
 
       <div className='footer__btn'>
           <button type='button'>Request Early Access</button>
+      </div>
+
+      <div className='footer__section-content'>
+
+        <div className='footer__section-content_gpt3'>
+          <img src={gpt3Logo} alt='logo'/>
+          <p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
+        </div>
+
+        <div className='footer__section-content_links'>
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+
+        <div className='footer__section-content_links'>
+          <h4>Company</h4>
+          <p>Terms & Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+
+        <div className='footer__section-content_links'>
+          <h4>Get in touch</h4>
+          <p>Crechterwoord K12 182 DK Alknjkcb</p>
+          <p>085-132567</p>
+          <p>info@payme.net</p>
+        </div>
+
+      </div>
+
+      <div className='footer__copyright'>
+        <p>© 2021 GPT-3. All rights reserved.</p>
+
       </div>
     </div>
   )
